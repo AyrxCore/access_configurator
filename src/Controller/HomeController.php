e5cbd0a9fdadc6a5112d61a3ba21160b899bd222
Ayrx <roux.anthony.ra@gmail.com>
2019-05-16 13:33:04 +0200

    init ddb + first entity

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,14 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\House\HouseModel;
+
 
 class HomeController extends AbstractController
 {
     /**
      * @Route ("/", name="home")
      */
-    public function hello()
+    public function homepage()
     {
+        $render = new HouseModel();
+        
+        dump($render->findAllQuery());
+        
         return $this->render('home.html.twig');
     }
 }
---
+++ src/Repository/HouseModelRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\House\HouseModel;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method HouseModel|null find($id, $lockMode = null, $lockVersion = null)
+ * @method HouseModel|null findOneBy(array $criteria, array $orderBy = null)
+ * @method HouseModel[]    findAll()
+ * @method HouseModel[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class HouseModelRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, HouseModel::class);
+    }
+
+    // /**
+    //  * @return HouseModel[] Returns an array of HouseModel objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('h')
+            ->andWhere('h.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('h.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?HouseModel
+    {
+        return $this->createQueryBuilder('h')
+            ->andWhere('h.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+    
+    public function findAllQuery()
+    {
+        return $this->createQueryBuilder('all')
+            ->getQuery()
+            ->getResult();
+    }
+}
