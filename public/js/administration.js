e3b975ac51fd5508a0ba30479e1dd700b3dd4252
AyrxCore <roux.anthony.ra@gmail.com>
2019-07-13 17:04:16 +0200

    some details admin + try edit when create

--- public/js/administration.js
+++ public/js/administration.js
@@ -10,10 +10,9 @@ $(document).ready(function() {
         e.preventDefault();
 
         let thisButton = $(this);
+        let form = thisButton.parents('form')[0];
 
-        console.log(thisButton.parents('form')[0])
-
-        let formData = new FormData(thisButton.parents('form')[0]);
+        let formData = new FormData(form);
         formData.append('type', thisButton.data('type'));
         formData.append('selectName', thisButton.parents('form').find('select').val())
 
@@ -25,28 +24,29 @@ $(document).ready(function() {
             processData: false,
             success: function(data)
             {
-                console.log(data)
+                console.log(Routing.generate('home'))
+                form.reset();
                 if(data['type'] === 'models'){
-                    let element = "<tr><td><a href=\"#\" class=\"editable name\" data-type=\"text\" data-pk="+ data.id +" data-url="+ data.path +" data-name=\"models\" data-pk="+ data.id +">"+ data.house_model.name +"</a></td><td><a href=\"#\" class=\"editable price\" data-pk="+ data.id +">"+ data.house_model.price +"</a></td><td><a href=\"#\" class=\"editable description\" data-pk="+ data.id +">"+ data.house_model.description +"</a></td><td><img class=\"imgModeleBase\" src=\"#\"></td><td class='list-actions'><button class=\"trash-element\" data-id="+ data.id +"><i class=\"fas fa-trash-alt\"></i></button></td></tr>";
+                    let element = "<tr><td><a href=\"#\" class=\"editable name\" data-type=\"text\" data-pk="+ data.id +" data-url="+ data.path +" data-name=\"models\">"+ data.house_model.name +"</a><i class=\"fas fa-pen-alt min-size\"></i></td><td><a href=\"#\" class=\"editable price\" data-pk="+ data.id +">"+ data.house_model.price +"</a><i class=\"fas fa-pen-alt min-size\"></i></td><td><a href=\"#\" class=\"editable description\" data-pk="+ data.id +">"+ data.house_model.description +"</a><i class=\"fas fa-pen-alt min-size\"></i></td><td><img class=\"imgModeleBase\" src=\"#\"></td><td class='list-actions'><button class=\"trash-element\" data-id="+ data.id +"><i class=\"fas fa-trash-alt\"></i></button></td></tr>";
                     thisButton.parents('.tab-pane.active').find('.model-list').append(element);
                     let newModelInSelect = "<option value="+ data.house_model.name +">"+data.house_model.name+"</option>";
                     thisButton.parents('.tab-content').find('#all-models').append(newModelInSelect);
                 }
 
                 if(data['type'] === 'surfaces'){
-                    let element = "<tr><td><a href=\"#\" class=\"editable name\" data-type=\"text\" data-pk="+ data.id +">"+ data.selectName +"</a></td><td><a href=\"#\" class=\"editable surface\" data-pk="+ data.id +">"+ data.house_size.surface +"</a></td><td><a href=\"#\" class=\"editable price\" data-pk="+ data.id +">"+ data.house_size.price +"</a></td><td><a href=\"#\" class=\"editable description\" data-pk="+ data.id +">"+ data.house_size.description +"</a></td><td><img class=\"imgModeleBase\" src=\"#\"></td><td class='list-actions'><button class=\"trash-element\" data-id="+ data.id +"><i class=\"fas fa-trash-alt\"></i></button></td></tr>";
+                    let element = "<tr><td><a href=\"#\" class=\"editable name\" data-type=\"text\" data-pk="+ data.id +">"+ data.selectName +"</a><i class=\"fas fa-pen-alt min-size\"></i></td><td><a href=\"#\" class=\"editable surface\" data-pk="+ data.id +">"+ data.house_size.surface +"</a><i class=\"fas fa-pen-alt min-size\"></i></td><td><a href=\"#\" class=\"editable price\" data-pk="+ data.id +">"+ data.house_size.price +"</a><i class=\"fas fa-pen-alt min-size\"></i></td><td><a href=\"#\" class=\"editable description\" data-pk="+ data.id +">"+ data.house_size.description +"</a><i class=\"fas fa-pen-alt min-size\"></i></td><td><img class=\"imgModeleBase\" src=\"#\"></td><td class='list-actions'><button class=\"trash-element\" data-id="+ data.id +"><i class=\"fas fa-trash-alt\"></i></button></td></tr>";
                     thisButton.parents('.tab-pane.active').find('.surface-list').append(element);
                 }
 
                 if(data['type'] === 'categories'){
-                    let element = "<tr><td><a href=\"#\" class=\"editable name\" data-type=\"text\" data-pk="+ data.id +">"+ data.category.name +"</a></td><td class='list-actions'><button class=\"trash-element\" data-id="+ data.id +"><i class=\"fas fa-trash-alt\"></i></button></td></tr>";
+                    let element = "<tr><td><a href=\"#\" class=\"editable name\" data-type=\"text\" data-pk="+ data.id +">"+ data.category.name +"</a><i class=\"fas fa-pen-alt min-size\"></i></td><td class='list-actions'><button class=\"trash-element\" data-id="+ data.id +"><i class=\"fas fa-trash-alt\"></i></button></td></tr>";
                     thisButton.parents('.tab-pane.active').find('.category-list').append(element);
                     let newCategoryInSelect = "<option value="+ data.category.name +">"+data.category.name+"</option>";
                     thisButton.parents('.tab-content').find('#all-categories').append(newCategoryInSelect);
                 }
 
                 if(data['type'] === 'options'){
-                    let element = "<tr><td><a href=\"#\" class=\"editable name\" data-type=\"text\" data-pk="+ data.id +">"+ data.options.name +"</a></td><td><a href=\"#\" class=\"editable category\" data-pk="+ data.id +">"+ data.selectName +"</a></td><td><a href=\"#\" class=\"editable price\" data-pk="+ data.id +">"+ data.options.price +"</a></td><td><a href=\"#\" class=\"editable description\" data-pk="+ data.id +">"+ data.options.description +"</a></td><td class='list-actions'><button class=\"trash-element\" data-id="+ data.id +"><i class=\"fas fa-trash-alt\"></i></button></td></tr>";
+                    let element = "<tr><td><a href=\"#\" class=\"editable name\" data-type=\"text\" data-pk="+ data.id +">"+ data.options.name +"</a><i class=\"fas fa-pen-alt min-size\"></i></td><td><a href=\"#\" class=\"editable category\" data-type=\"select\" data-pk="+ data.id +">"+ data.selectName +"</a><i class=\"fas fa-pen-alt min-size\"></i></td><td><a href=\"#\" class=\"editable price\" data-pk="+ data.id +">"+ data.options.price +"</a><i class=\"fas fa-pen-alt min-size\"></i></td><td><a href=\"#\" class=\"editable description\" data-pk="+ data.id +">"+ data.options.description +"</a><i class=\"fas fa-pen-alt min-size\"></i></td><td class='list-actions'><button class=\"trash-element\" data-id="+ data.id +"><i class=\"fas fa-trash-alt\"></i></button></td></tr>";
                     thisButton.parents('.tab-pane.active').find('.option-list').append(element);
                 }
             }
